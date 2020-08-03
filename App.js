2154ec29919e02578bc26784961a466cd2ec91cf
nhattranminh <trannhat2411999@gmail.com>
2020-08-03 17:00:19 +0700

    learning React Native

---
+++ App.js
@@ -0,0 +1,217 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useEffect, useState } from 'react';
+import ToastExample from './js/ToastExample';
+
+
+
+// import React from 'react';
+import {
+  SafeAreaView,
+  ActivityIndicator,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  Button,
+  Image,
+  View,
+  Text,
+  StatusBar,
+  ToastAndroid,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  container: {
+    flex: 1,
+    paddingTop: 22
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+   },
+   imageView: {
+ 
+    width: '50%',
+    height: 100 ,
+    margin: 7,
+    borderRadius : 7
+ 
+}
+});
+
+
+const Stack = createStackNavigator();
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="BookList" component={BookListScreen} />
+        <Stack.Screen name="BookDetail" component={BookDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Load Book Api"
+      onPress={() =>
+        
+        navigation.navigate('BookList')
+      }
+    />
+  );
+};
+const BookDetailScreen =()=>{
+  
+  return (
+  <Text >"TEST"</Text>
+  )
+}
+const BookListScreen = ({ navigation }) => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=harry')
+      .then((response) => response.json())
+      .then((json) => setData(json.items))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));  
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={()=>
+              navigation.navigate('BookDetail',{ description: item.volumeInfo.description })
+          }   style={{flex:1, flexDirection: 'row'} }>
+    
+            <Image  source = {{ uri: item.volumeInfo.imageLinks.smallThumbnail }} style={styles.imageView} />
+            <Text style={styles.textView} >{item.volumeInfo.title}</Text>
+ 
+
+          </TouchableOpacity>
+            )}
+        />
+      )}
+    </View>
+  );
+};
+export default MyStack;
